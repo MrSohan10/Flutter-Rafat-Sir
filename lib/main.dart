32abec6e4906aa81bac5e27d0fcbee12505e58b3
MrSohan10 <iamsohan11@gmail.com>
2023-09-12 23:00:06 +0600

    practice gestureDetector and InkWell

--- lib/main.dart
+++ lib/main.dart
@@ -6,79 +6,38 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  int _currentIndex = 1;
-  mySnackbar(message, context) {
-    return ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
-
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Home'),
-          ),
-          body: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-              ElevatedButton(
-              style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.all(20),
-              backgroundColor: Colors.pink,
-              textStyle:
-              TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20))),
-          onPressed: () {
-            mySnackbar('sohan', context);
-          },
-          child: Text('Button')),
-      TextButton(
-          style: TextButton.styleFrom(
-              padding: EdgeInsets.all(20),
-              foregroundColor: Colors.teal,
-              textStyle: TextStyle(
-                fontSize: 20,
-              )),
-          onPressed: () {},
-          child: Text('Forword')),
-      IconButton(
-          style: IconButton.styleFrom(),
-          iconSize: 50,
-          onPressed: () {},
-          icon: Icon(
-            Icons.person,
-          )),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          decoration: InputDecoration(
-              border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              suffixIcon: Icon(Icons.mail),
-          hintText: 'Enter your email',
-              label: Text("Enter mail")
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onLongPress: (){
+                  print('Long press');
+                },
+                onTap: (){
+                  print('Hello world');
+                },
+                child: Text('Hello world'),
+              ),
+              InkWell(
+                splashColor: Colors.green,
+                borderRadius: BorderRadius.circular(10),
+                onTap: (){
+                  print('Hello wrld 2');
+                },
+                child: Text('Hello world 2'),
+              )
+            ],
           ),
         ),
       ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              suffixIcon: Icon(Icons.password),
-              hintText: 'Enter your password',
-            label: Text("Enter password")
-
-          ),
-
-        ),
-      ),
-
-    ],
-    )),
-    ));
+    );
   }
 }
